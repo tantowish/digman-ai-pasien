59ff14bf00b59b8692e635acaa470b448dc62ebf
tanto <tantows001@gmail.com>
2024-08-16 14:23:03 +0700

    feat: appointment button

--- src/components/appointment-button.tsx
+++ src/components/appointment-button.tsx
@@ -1,7 +1,16 @@
+import { useContext } from "react";
 import { Button } from "./ui/button";
+import { Context } from "@/context/context";
 
 export default function AppointmentButton() {
+  const {resume, imageResume, uploadedFile, params} = useContext(Context)
+  const link = `https://${params?.link}/register?rekmed=${params?.rekmed}&rangkuman=${resume}&rangkumanGambar=${imageResume}&image=${uploadedFile}`
+  
+  const handleClick = () => {
+    window.open(link, '_blank');
+  };
+
   return (
-    <Button className="bg-main text-white py-2 px-4 lg:py-2.5 lg:px-8 text-sm md:text-base rounded-md hover:bg-teal-400 transition duration-300 flex items-center">Daftar Antrian</Button>
+    <Button onClick={handleClick} className={`bg-main text-white py-2 px-4 lg:py-2.5 lg:px-8 text-sm md:text-base rounded-md hover:bg-teal-400 transition duration-300 flex items-center`} disabled={params?.rekmed == undefined || params.rekmed.trim() == ""}>Daftar Antrian</Button>
   )
 }
